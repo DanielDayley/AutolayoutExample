d0e8212c96a4534468ceeb47c690cffd2f754f0b
Daniel Dayley <danieldayley@aim.com>
2015-05-08 16:19:23 -0600

    Enabled Stepper functionailty
    While the values of the scores are currently set and editable, the value only gets set to the value of the stepper.

--- AutolayoutExample/AutolayoutExample/AETableViewCell.m
+++ AutolayoutExample/AutolayoutExample/AETableViewCell.m
@@ -14,10 +14,7 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     
     self.photoButton = [UIButton new];
-<<<<<<< HEAD
-=======
-//    self.photoButton.frame = CGRectMake(margin, 6, 48, 48);
->>>>>>> initial
+    //    self.photoButton.frame = CGRectMake(margin, 6, 48, 48);
     [self.photoButton setTitle:@"Add" forState:UIControlStateNormal];
     [self.photoButton setTintColor:[UIColor whiteColor]];
     [self.photoButton setBackgroundColor:[UIColor blueColor]];
@@ -25,58 +22,47 @@
     [self.contentView addSubview:self.photoButton];
     
     self.nameField = [UITextField new];
-<<<<<<< HEAD
-=======
-//    self.nameField.frame = CGRectMake(margin + 48 + margin, 6, 125, 48);
->>>>>>> initial
+    //    self.nameField.frame = CGRectMake(margin + 48 + margin, 6, 125, 48);
     self.nameField.borderStyle = UITextBorderStyleRoundedRect;
     [self.contentView addSubview:self.nameField];
     
     self.scoreField = [UITextField new];
-<<<<<<< HEAD
-=======
-//    self.scoreField.frame = CGRectMake(margin + 48 + margin + 125 + margin, 6, 55, 48);
->>>>>>> initial
+    //    self.scoreField.frame = CGRectMake(margin + 48 + margin + 125 + margin, 6, 55, 48);
     self.scoreField.borderStyle = UITextBorderStyleRoundedRect;
     [self.contentView addSubview:self.scoreField];
     
     self.scoreStepper = [UIStepper new];
-<<<<<<< HEAD
-=======
-//    self.scoreStepper.frame = CGRectMake(margin + 48 + margin + 125 + margin + 55 + margin, 15, 0, 0);
->>>>>>> initial
+    //    self.scoreStepper.frame = CGRectMake(margin + 48 + margin + 125 + margin + 55 + margin, 15, 0, 0);
+    // Configure the stepper to send a message to change the label
+    [self.scoreStepper addTarget:self action:@selector(changeLabelAtIndex:) forControlEvents:UIControlEventValueChanged];
+    self.scoreStepper.tag = 12;
     [self.contentView addSubview:self.scoreStepper];
     
     // CONSTRAINTS
     
-<<<<<<< HEAD
     // Turn off Autoresizing Mask Layout Constraints that are on by default
-=======
->>>>>>> initial
     [self.photoButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.nameField setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.scoreField setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.scoreStepper setTranslatesAutoresizingMaskIntoConstraints:NO];
-<<<<<<< HEAD
     
     // Make a dictionary of all the views you need to lay out
-    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(_photoButton, _nameField, _scoreField, _scoreStepper);
+    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(_photoButton,_nameField,_scoreField,_scoreStepper);
     
-    // Space everything out by default spaces with flexible space between score and stepper with min width on name and score
+    // The visual format of setting restraints.
     NSArray *constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_photoButton]-[_nameField(>=95)]-[_scoreField(>=40)]-(>=8)-[_scoreStepper]-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:viewsDictionary];
-
-    // Add the constraint to the common ancestor of all the views you want to lay out
+    
     [self.contentView addConstraints:constraints];
     
-    // Try to center the score
-    NSLayoutConstraint *actualScoreCenterConstraint =
-    [NSLayoutConstraint constraintWithItem:_scoreField
-                                 attribute:NSLayoutAttributeCenterX
-                                 relatedBy:NSLayoutRelationEqual
-                                    toItem:self.contentView
-                                 attribute:NSLayoutAttributeCenterX
-                                multiplier:1.0
-                                  constant:0.0];
+    //Apple's base-API way of adding restraints
+    NSLayoutConstraint *actualScoreCenterConstraint = [NSLayoutConstraint
+                                                       constraintWithItem:_scoreField
+                                                       attribute:NSLayoutAttributeCenterX
+                                                       relatedBy:NSLayoutRelationEqual
+                                                       toItem:self.contentView
+                                                       attribute:NSLayoutAttributeCenterX
+                                                       multiplier:1.0
+                                                       constant:0];
     actualScoreCenterConstraint.priority = UILayoutPriorityDefaultHigh;
     [self.contentView addConstraint:actualScoreCenterConstraint];
     
@@ -94,37 +80,16 @@
                                 multiplier:1.0
                                   constant:0];
     [self.photoButton addConstraint:photoButtonAspectRatioConstraint];
-    
-=======
 
-    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(_photoButton,_nameField,_scoreField,_scoreStepper);
-    
-    // The visual format of setting restraints.
-    
-    NSArray *constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_photoButton]-[_nameField(>=95)]-[_scoreField(>=40)]-(>=8)-[_scoreStepper]-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:viewsDictionary];
-    
-    [self.contentView addConstraints:constraints];
-    
-    //Apple's base-API way of adding restraints
-    
-    NSLayoutConstraint *actualScoreCenterConstraint = [NSLayoutConstraint
-                                                       constraintWithItem:_scoreField
-                                                       attribute:NSLayoutAttributeCenterX
-                                                       relatedBy:NSLayoutRelationEqual
-                                                       toItem:self.contentView
-                                                       attribute:NSLayoutAttributeCenterX
-                                                       multiplier:1.0
-                                                       constant:0];
-    actualScoreCenterConstraint.priority = UILayoutPriorityDefaultHigh;
-    [self.contentView addConstraint:actualScoreCenterConstraint];
-
-    
-    // since we've already rendered constraints, we can reuse that variable:
+    // Since we've already rendered constraints, we can reuse that variable:
     constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[_photoButton]-|" options:0 metrics: nil views:viewsDictionary];
         [self.contentView addConstraints:constraints];
 
->>>>>>> initial
     return self;
+}
+
+- (void)changeLabelAtIndex:(UIStepper *)sender {
+    [self.scoreField setText:[NSString stringWithFormat:@"%d", (int)sender.value]];
 }
 
 - (void)drawRect:(CGRect)rect {
